39e4dc660efe4013d997cdc4705620a97c28fae9
Saulo Tauil <saulogt@gmail.com>
2023-10-27 11:34:30 -0700

    ...

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,5 +1,8 @@
 // Importing env files here to validate on build
 import "./src/env.mjs";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -9,6 +12,18 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
+    config.externals = [...config.externals, { canvas: "canvas" }]; // required to make Konva & react-konva work
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return config;
+  },
 };
 
 export default config;
