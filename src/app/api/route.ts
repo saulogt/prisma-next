468fee2f4c6bb504620ecbe6b356f6416f4d4e0a
Saulo Tauil <saulogt@gmail.com>
2023-10-26 18:26:37 -0700

    ...

---
+++ src/app/api/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+const id = "dfgdfg";
+const orgId = "eLk7SSltmbQzX5flmU92hBlK2Md2";
+
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as { prisma?: PrismaClient };
+
+export const db =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
+export async function GET(request: NextRequest, { params }: any) {
+  const patient = await db.client.findUnique({
+    where: { orgId, id },
+    //
+  });
+
+  console.log(patient);
+
+  return NextResponse.json(patient);
+}
